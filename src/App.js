2758e3c9d2d14c856c9df16232f72dade53735f7
antoliny0919 <antoliny0919@gmail.com>
2025-10-15 11:29:44 +0900

    feat(계산기): 기본 구분자 처리 로직 추가

--- src/App.js
+++ src/App.js
@@ -2,8 +2,24 @@ import { Console } from "@woowacourse/mission-utils";
 
 
 class App {
+  constructor() {
+    this.delimiter = [',', ':'];
+  }
+
+  checkCustomDelimiter(value) {
+    if (value.startsWith('//')) {
+      return true;
+    }
+    return false;
+  }
+
   async run() {
     const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+    if (this.checkCustomDelimiter(input)) {
+      // 커스텀 구분자 사용
+    } else {
+
+    }
   }
 }
 
