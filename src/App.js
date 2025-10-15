ea94665db747430de673ac66ac0413d77c9d8fe7
antoliny0919 <antoliny0919@gmail.com>
2025-10-15 23:23:03 +0900

    feat(계산기): 계산할 문자열 값 처리로직 추가
    문자열 계산기에 계산할 문자열 값을 처리하는 로직을 추가했습니다:
    - Raw상태인 계산할 문자열을 계산가능한 상태로 만드는 로직 추가
    - 구분자를 기준으로 계산할 문자열을 분리하는 로직 추가
    - 구분자를 기준으로 분리된 각 요소가 숫자인지 확인하는 로직 추가
    - 구분자를 기준으로 분리된 각 요소가 양수인지 확인하는 로직 추가
    - 계산할 문자열 입력 예외 테스트 케이스 추가

--- src/App.js
+++ src/App.js
@@ -15,6 +15,12 @@ class App {
     return { 'customDelimiter': undefined, 'calculationString': value }
   }
 
+  makeCalculable(calculationString) {
+    const standard = new RegExp(`[${this.delimiter.join('')}]`);
+    const tokens = calculationString.split(standard);
+    this.validateCalculationString(tokens);
+  }
+
   validateCustomDelimiter(delimiter) {
     this.validateIsNotNumber(delimiter);
     this.validateIsChar(delimiter);
@@ -32,6 +38,25 @@ class App {
     }
   }
 
+  validateCalculationString(value) {
+    this.validateFormat(value);
+    this.validateIsAllPositive(value);
+  }
+
+  validateFormat(value) {
+    // 배열 내 모든 요소가 숫자인지 확인
+    if (!value.every(el => /^-?\d+$/.test(el))) {
+      throw new Error('[ERROR] 계산할 문자열이 잘못된 형식입니다.');
+    }
+  }
+
+  validateIsAllPositive(value) {
+    // 배열 내 모든 요소가 숫자인지 확인
+    if (!value.every(el => Number(el) > 0)) {
+      throw new Error('[ERROR] 계산할 문자열은 양수만 가능합니다.');
+    }
+  }
+
   async run() {
     const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
     const { customDelimiter, calculationString } = this.getCustomDelimiter(input);
@@ -39,6 +64,7 @@ class App {
       // 커스텀 구분자 사용
       this.delimiter.push(customDelimiter);
     }
+    this.makeCalculable(calculationString);
   }
 }
 
