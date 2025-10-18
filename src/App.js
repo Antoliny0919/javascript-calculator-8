e0631c7ef3a3e5f53f310a19c291498a82e73cdb
antoliny0919 <antoliny0919@gmail.com>
2025-10-18 09:50:29 +0900

    refactor(계산기): 계산기 클래스 추가, 계산기 로직 분리

--- src/App.js
+++ src/App.js
@@ -1,73 +1,12 @@
+import Calculator from "./Calculator.js";
 import { Console } from "@woowacourse/mission-utils";
-import { ERROR_MESSAGES, MESSAGES } from "./Constants.js";
-import { RegexpValidator, LengthValidator, ArrayElementsValidator } from "./Validators.js";
-
+import { MESSAGES } from "./Constants.js";
 
 class App {
-  constructor() {
-    this.delimiter = [',', ':'];
-    this.validators = {
-      customDelimiter: [
-        new RegexpValidator(/\d/, ERROR_MESSAGES.INVALID_CUSTOM_DELIMITER_NUMBER),
-        new LengthValidator(0, 2, ERROR_MESSAGES.INVALID_CUSTOM_DELIMITER_STRING),
-      ],
-      calculationString: [
-        // 배열 내 모든 요소가 숫자인지 확인
-        new ArrayElementsValidator(
-          el => /^-?\d+$/.test(el),
-          ERROR_MESSAGES.INVALID_FORMAT,
-        ),
-        // 배열 내 모든 요소가 양수인지 확인
-        new ArrayElementsValidator(
-          el => Number(el) > 0,
-          ERROR_MESSAGES.INVALID_NEGATIVE_OR_ZERO,
-        ),
-      ]
-    }
-  }
-
-  addDelimiter(delimiter) {
-    let newDelimiter = delimiter;
-    if (newDelimiter === '\\') {
-      newDelimiter = '\\\\';
-    }
-    this.delimiter.push(newDelimiter);
-  }
-
-  runValidators(value, key) {
-    for (const validators of this.validators[key]) {
-      validators.validate(value);
-    }
-  }
-
-  getCustomDelimiter(value) {
-    if (value.startsWith('//')) {
-      const [ delimiter, calculationString ] = value.slice(2,).split('\\n');
-      this.runValidators(delimiter, 'customDelimiter');
-      return { 'customDelimiter': delimiter, 'calculationString': calculationString };
-    }
-    return { 'customDelimiter': undefined, 'calculationString': value };
-  }
-
-  makeCalculable(calculationString) {
-    if (calculationString === "") {
-      return [];
-    }
-
-    const standard = new RegExp(`[${this.delimiter.join('')}]`);
-    const tokens = calculationString.split(standard);
-    this.runValidators(tokens, 'calculationString');
-    return tokens.map(el => Number(el));
-  }
-
   async run() {
     const input = await Console.readLineAsync(MESSAGES.INPUT);
-    const { customDelimiter, calculationString } = this.getCustomDelimiter(input);
-    if (customDelimiter) {
-      this.addDelimiter(customDelimiter);
-    }
-    const tokens = this.makeCalculable(calculationString);
-    const result = tokens.reduce((x, y) => x + y, 0);
+    const calculator = new Calculator(input);
+    const result = calculator.calculate();
     Console.print(MESSAGES.OUTPUT(result));
   }
 }
---
+++ src/Calculator.js
@@ -0,0 +1,73 @@
+import { RegexpValidator, LengthValidator, ArrayElementsValidator } from './Validators';
+import { ERROR_MESSAGES } from './Constants';
+
+class Calculator {
+  constructor(input) {
+    this.input = input;
+    this.delimiter = [',', ':'];
+    this.validators = {
+      customDelimiter: [
+        new RegexpValidator(/\d/, ERROR_MESSAGES.INVALID_CUSTOM_DELIMITER_NUMBER),
+        new LengthValidator(0, 2, ERROR_MESSAGES.INVALID_CUSTOM_DELIMITER_STRING),
+      ],
+      calculationString: [
+        // 배열 내 모든 요소가 숫자인지 확인
+        new ArrayElementsValidator(
+          el => /^-?\d+$/.test(el),
+          ERROR_MESSAGES.INVALID_FORMAT,
+        ),
+        // 배열 내 모든 요소가 양수인지 확인
+        new ArrayElementsValidator(
+          el => Number(el) > 0,
+          ERROR_MESSAGES.INVALID_NEGATIVE_OR_ZERO,
+        ),
+      ]
+    }
+  }
+
+  addDelimiter(delimiter) {
+    let newDelimiter = delimiter;
+    if (newDelimiter === '\\') {
+      newDelimiter = '\\\\';
+    }
+    this.delimiter.push(newDelimiter);
+  }
+
+  runValidators(value, key) {
+    for (const validators of this.validators[key]) {
+      validators.validate(value);
+    }
+  }
+
+  getCustomDelimiter(value) {
+    if (value.startsWith('//')) {
+      const [ delimiter, calculationString ] = value.slice(2,).split('\\n');
+      this.runValidators(delimiter, 'customDelimiter');
+      return { 'customDelimiter': delimiter, 'calculationString': calculationString };
+    }
+    return { 'customDelimiter': undefined, 'calculationString': value };
+  }
+
+  makeCalculable(calculationString) {
+    if (calculationString === "") {
+      return [];
+    }
+
+    const standard = new RegExp(`[${this.delimiter.join('')}]`);
+    const tokens = calculationString.split(standard);
+    this.runValidators(tokens, 'calculationString');
+    return tokens.map(el => Number(el));
+  }
+
+  calculate() {
+    const { customDelimiter, calculationString } = this.getCustomDelimiter(this.input);
+    if (customDelimiter) {
+      this.addDelimiter(customDelimiter);
+    }
+    const tokens = this.makeCalculable(calculationString);
+    const result = tokens.reduce((x, y) => x + y, 0);
+    return result;
+  }
+}
+
+export default Calculator;
